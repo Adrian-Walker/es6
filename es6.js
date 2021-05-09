64c6606c99fd222e0db1f26ae76ba0caa8981aae
Adrian Walker <awalker3365@gmail.com>
2021-05-09 19:52:12 -0400

    es6 1

---
+++ es6.js
@@ -0,0 +1,8 @@
+// #1.)
+const carrots = ["bright orange", "ripe", "rotten"]
+
+function mapVegetables(arr) {
+    return arr.map(function(carrot) {
+        return { type: "carrot", name: carrot }
+    })
+}
