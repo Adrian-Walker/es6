4d45119a80bcd407ace87ae3aae380e10e580db9
Adrian Walker <awalker3365@gmail.com>
2021-05-10 21:13:16 -0400

    next 1

--- es6.js
+++ es6.js
@@ -1,8 +1,49 @@
 // #1.)
 const carrots = ["bright orange", "ripe", "rotten"]
 
-function mapVegetables(arr) {
-    return arr.map(function(carrot) {
+let mapVegetables = (arr) => {
+    return arr.map((carrot) => {
         return { type: "carrot", name: carrot }
     })
+}
+
+// #2.)
+const people = [
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
+]
+
+let filterForFriendly = (arr) => {
+    arr.filter((person) => {
+        person.friendly
+    })
+}
+
+// #3.)
+
+let doMathSum = (a, b) => {
+    return a + b
+}
+
+let produceProduct = (a, b) => {
+    return a * b
+}
+
+// #4.)
+let printName = () => {
+    
 }
